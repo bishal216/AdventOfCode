fe13a2c5b31ac02070464dd4e5dfd4f06d09cecf
Bishal Katuwal <bis3hal@gmail.com>
2023-05-19 09:58:07 +0545

    updated
    updated

--- AoC2022/Day7.py
+++ AoC2022/Day7.py
@@ -2,26 +2,46 @@
 # Input
 lines = open('AoC2022/inputs/Day7.txt','r').readlines()
 
+# Create a file/folder class 
 class dir:
-    name =''
-    size = 0.0
-    parentDir = None
-    def __init__(self, name,size,parentDir):
+    def __init__(self, name,parentDir,size,type):
         self.name = name
-        self.size = size
         self.parentDir = parentDir
+        self.size = size
+        self.type = type
+    def __str__(self):
+        print(self.name)
 
-rootdir = dir('\\',0,None)
-Dirlist =[rootdir]
-# Process
-for line in lines:
-    line = line.rstrip()
-    if (line =='$ ls'):
-        continue
-    if (line[0] =='$'):
-        line = line[1:]
-    
-    w1,w2 = line.split()
-    print(w1+' '+w2)
-print(Dirlist[0].__getattribute__())
+cDir = lambda itemList, name: next((item for item in itemList if item['name'] == name), None)
+
+def main():
+    rootdir = dir('/',None,0,'Folder')
+    currentDir = rootdir
+    Dirlist = set()
+    Dirlist.add(rootdir)
+    # Process
+    for line in lines:
+        line = line.rstrip()
+        if (line =='$ ls'):
+            continue
+        else: 
+            line = line.split()
+            if(line[0]=='$'):
+                line = line[1:]
+            
+            if(line[0]=='cd'):
+                print(currentDir)
+                if(line[1]=='/'):
+                    currentDir = rootdir
+                elif(line[1]=='..'):
+                    currentDir = currentDir.parentDir
+                else:
+                    currentDir = cDir(Dirlist,line[2])
+            elif(line[0]=='dir'):
+                Dirlist.add(dir('line[1]',0,currentDir,'Folder'))
+                   
+main()  
+#     w1,w2 = line.split()
+#     print(w1+' '+w2)
+# print(Dirlist[0].__getattribute__())
     
--- AoC2022/Day9.py
+++ AoC2022/Day9.py
@@ -1,16 +1,10 @@
 # Input
 lines = open('AoC2022/inputs/Day9.txt').readlines()
-
 # Program
-# ---------------------------------------------------
 Tailvisited={(0,0)}
 headpos =(0,0)
 tailpos = (0,0)
-
-Part2TailVisited =  {(0,0)}
-rope = [(0,0)]*10
-# =============================================
-def Part1(hPos,tPos,dir,steps,addToTail,part):
+def Part1(hPos,tPos,dir,steps):
     offset=(0,0)
     if(dir == 'L'):
         offset = (-1,0)
@@ -22,27 +16,15 @@ def Part1(hPos,tPos,dir,steps,addToTail,part):
          offset = (0,-1)
     for i in range(int(steps)):
         hPos = (hPos[0]+offset[0],hPos[1]+offset[1])
-        # if(hPos == tPos):
-        #     return hPos,tPos
         Hx,Hy = hPos
         Tx,Ty = tPos
         newTailpos = (Tx + (Hx>Tx)-(Hx<Tx),Ty + (Hy>Ty)-(Hy<Ty))
         if(hPos != newTailpos):
             tPos = newTailpos
-            if(part==1):
-                Tailvisited.add(tPos)
-            elif(part==2 and addToTail==True):
-                Part2TailVisited.add(tPos)
+            Tailvisited.add(tPos)
     return hPos,tPos
-#-----------------------------------------
-
 for line in lines:
     dir,steps = line.rstrip().split()
-    headpos,tailpos = Part1(headpos,tailpos,dir,steps,True,1)
-
-    for i,knot in enumerate(rope):
-        if(i != 9):
-            rope[i],rope[i+1] = Part1(rope[i],rope[i+1],dir,steps,i==8,2)
-
+    headpos,tailpos = Part1(headpos,tailpos,dir,steps)
+#Output 
 print(len(Tailvisited))
-print(len(Part2TailVisited)) 
