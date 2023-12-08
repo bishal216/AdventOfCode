6aafbe38bfc75c3d16c44956f240f38f0f6b8b55
Bellatrix <bis3hal@gmail.com>
2023-12-09 00:19:16 +0545

    day8 p2 2023`
    tacky

--- AoC2023/day8.py
+++ AoC2023/day8.py
@@ -1,5 +1,5 @@
 lines = open('Aoc2023/inputs/day8.txt', 'r').readlines()
-# lines = open('Aoc2023/inputs/test.txt', 'r').readlines()
+
 def parse_lines(lines):
     instruction = lines[0].strip()
     instruction_set = {}
@@ -7,10 +7,7 @@ def parse_lines(lines):
         node, next = line.split('=')
         node = node.strip()
         left, right = next.split(',')
-        left = left.strip()[1:]
-        right = right.strip()[:-1]
-        instruction_set[node] = (left, right)
-    print(len(set(list(instruction_set.keys()) + list(instruction_set.values()))) )
+        instruction_set[node] = (left.strip()[1:], right.strip()[:-1])
     return instruction, instruction_set
 
 def part1(lines):
@@ -22,20 +19,30 @@ def part1(lines):
         count += 1
     return count
 
-def part2(lines):
-    instruction, instruction_set = parse_lines(lines)
+def get_cycles(value, instruction, instruction_set):
     count = 0
-    nodes = [node for node in list(instruction_set.keys()) if node[-1] =='A']
-    cycle = [[0]] * len(nodes)
-    # print(nodes)
-    while not all([node[-1] == 'Z' for node in nodes]):
-        for i, node in enumerate(nodes):
-            nodes[i] = instruction_set[node][int(instruction[count % len(instruction)] == 'R')]
-            if (nodes[i])[-1] == 'Z':
-                (cycle[i]).append(count - (cycle[i])[-1])
-                print(cycle)
+    while value[-1] != 'Z':
+        value = instruction_set[value][int(instruction[count % len(instruction)] == 'R')]
         count += 1
     return count
+
+def gcd(a, b):
+    return max(a, b) if min(a, b) == 0 else gcd(max(a, b) % min(a, b), min(a, b))
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+def lcm_array(array):
+    base = array[0]
+    for i in array[1:]:
+        base = lcm(base, i)
+    return base
+    
+def part2(lines):
+    instruction, instruction_set = parse_lines(lines)
+    nodes = [node for node in list(instruction_set.keys()) if node[-1] =='A']
+    cycle_length = [get_cycles(node, instruction, instruction_set) for node in nodes]
+    return lcm_array(cycle_length)
+
 print(part1(lines))
 print(part2(lines))
-# print(parse_lines(lines))
