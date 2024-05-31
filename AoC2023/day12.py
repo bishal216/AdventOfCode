a9445f13cf43c03da876e06b2a6a11be9d80777c
Bellatrix <bis3hal@gmail.com>
2024-05-31 20:46:27 +0545

    Update day12.py

--- AoC2023/day12.py
+++ AoC2023/day12.py
@@ -8,9 +8,28 @@ def separate_configs(lines):
         a, b = line.split(' ')
         configs.append((list(filter(lambda chunk: chunk != '', a.split('.'))), (list(map(int, b.split(','))))))
     return configs
+
+def get_configs(config):
+    possible_configs = []
+    conf1, conf2 = config
+    while conf1 and conf2 and len(conf1[0]) == conf2[0]:
+        conf1 = conf1[1:]
+        conf2 = conf2[1:]
+    while conf1 and conf2 and len(conf1[-1]) == conf2[-1]:
+        conf1 = conf1[:-1]
+        conf2 = conf2[:-1]
+    if not conf1 and not conf2:
+        possible_configs.append(config[0])
+    print(conf1, conf2)
+    print(possible_configs)
+    print('=========================')
+    return possible_configs
 def part1(lines):
     configs = separate_configs(lines)
-    return configs
+    config_count = 0
+    for config in configs:
+        config_count += len(get_configs(config))
+    return config_count
 
 
 print(part1(lines))
